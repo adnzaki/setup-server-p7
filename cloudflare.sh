a3150d0811cd3cc3717b83f278a9b6bbb4110753
Adnan Zaki <adnzaki@outlook.com>
2025-10-16 20:12:22 +0700

    turn off cloudflare service first before running installation

--- cloudflare.sh
+++ cloudflare.sh
@@ -1,3 +1,6 @@
+# 1. Pastikan service cloudflare mati
+cloudflared service uninstall
+
 # 2. Tentukan user aktif dan buat folder credential
 ACTIVE_USER=${SUDO_USER:-$(whoami)}
 USER_HOME=$(eval echo "~$ACTIVE_USER")
