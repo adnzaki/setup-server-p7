e01a237d43e28f02dbc5f09f21a812fa7b54f6e6
Adnan Zaki <adnzaki@outlook.com>
2025-11-07 10:45:38 +0700

    fix tailscale installation

--- packages/tailscale.sh
+++ packages/tailscale.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# 1. Tambahkan repository Tailscale untuk Ubuntu Noble
-curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/noble.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
-curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/noble.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
-
-# 2. Update dan install Tailscale
-sudo apt-get update
-sudo apt-get install tailscale
-
-sudo tailscale up
+# 1. Install dengan One command install dari tailscale
+curl -fsSL https://tailscale.com/install.sh | sh
 
 # 3. Enable dan start service Tailscale
 sudo systemctl enable tailscaled
