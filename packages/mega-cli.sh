cbdd417830de940b13be43c800905e57ad414e34
Adnan Zaki <adnzaki@outlook.com>
2025-11-07 10:38:02 +0700

    fix megacmd installation

--- packages/mega-cli.sh
+++ packages/mega-cli.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # 1. Tambahkan repository MEGA
-wget https://mega.nz/linux/repo/Debian_13/amd64/megacmd-Debian_13_amd64.deb && sudo apt install "$PWD/megacmd-Debian_13_amd64.deb"
+wget https://mega.nz/linux/repo/xUbuntu_24.04/amd64/megacmd-xUbuntu_24.04_amd64.deb && sudo apt install "$PWD/megacmd-xUbuntu_24.04_amd64.deb"
 
 # 2. Install megacmd
-sudo apt install -y ./megacmd-Debian_13_amd64.deb
-sudo apt --fix-broken install
 echo "Veri Mega yg diinstal:"
 mega-version
 
