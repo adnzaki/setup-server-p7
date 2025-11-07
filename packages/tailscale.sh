3391b60f881e1c6642b156507d9c30909fef1c59
Adnan Zaki <adnzaki@outlook.com>
2025-11-07 10:51:58 +0700

    fix tailscale service

--- packages/tailscale.sh
+++ packages/tailscale.sh
@@ -3,6 +3,9 @@
 # 1. Install dengan One command install dari tailscale
 curl -fsSL https://tailscale.com/install.sh | sh
 
+# 2. Jalankan daemon Tailscale
+sudo tailscale up
+
 # 3. Enable dan start service Tailscale
 sudo systemctl enable tailscaled
 sudo systemctl start tailscaled
