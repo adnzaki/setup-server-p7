7d391c72fa43a9e1ce5e431b0a6779c34461fc5e
Adnan Zaki <adnzaki@outlook.com>
2025-11-12 10:36:13 +0700

    fix chmod

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 echo "🚀🚀🚀 Memulai update..."
 
 git fetch && git pull
-chmod +x /home/admin1/setup-server-p7/*.sh
+chmod +x /home/admin1/setup-server-p7/backup/*.sh
 
 echo "✅🏁Update selesai. Permission telah diperbarui."
