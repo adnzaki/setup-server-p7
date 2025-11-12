7846e9cafc89d677df202f171900ff7dd588df21
Adnan Zaki <adnzaki@outlook.com>
2025-11-12 10:26:57 +0700

    disable bitdanbait-tunnel installation

--- install.sh
+++ install.sh
@@ -29,9 +29,9 @@ echo "🚀 Memasang Cloudflare untuk keperluan tunnel..."
 bash packages/cloudflare.sh
 echo "✅ Instalasi Cloudflare selesai"
 
-echo "🚀 Mengonfigurasi tunnel untuk Bitdanbait..."
-bash packages/bitdanbait-tunnel.sh
-echo "✅ Konfigurasi tunnel untuk Bitdanbait selesai"
+# echo "🚀 Mengonfigurasi tunnel untuk Bitdanbait..."
+# bash packages/bitdanbait-tunnel.sh
+# echo "✅ Konfigurasi tunnel untuk Bitdanbait selesai"
 
 echo "🚀 Memasang MEGA CLI..."
 bash packages/mega-cli.sh
