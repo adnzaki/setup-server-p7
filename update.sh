61b1143cbd52eb554941d4893e41ab058f219ae1
Adnan Zaki <adnzaki@outlook.com>
2025-11-12 10:36:44 +0700

    add whitespace

--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@ echo "🚀🚀🚀 Memulai update..."
 git fetch && git pull
 chmod +x /home/admin1/setup-server-p7/backup/*.sh
 
-echo "✅🏁Update selesai. Permission telah diperbarui."
+echo "✅🏁 Update selesai. Permission telah diperbarui."
