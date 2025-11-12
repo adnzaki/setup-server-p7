b339a3fe70e7a952ea09b5a041df493b92d55560
Adnan Zaki <adnzaki@outlook.com>
2025-11-12 10:41:59 +0700

    fix permission update

--- update.sh
+++ update.sh
@@ -1,6 +1,10 @@
 echo "🚀🚀🚀 Memulai update..."
 
-git fetch && git pull
-chmod +x /home/admin1/setup-server-p7/backup/*.sh
+ACTIVE_USER=${SUDO_USER:-$(whoami)}
+USER_HOME=$(eval echo "~$ACTIVE_USER")
+BACKUP_FOLDER=$USER_HOME/setup-server-p7/backup
 
-echo "✅🏁 Update selesai. Permission telah diperbarui."
+git fetch && git pull
+chmod +x $BACKUP_FOLDER/*.sh
+
+echo "✅🏁 Update selesai. Permission pada $BACKUP_FOLDER telah diperbarui."
