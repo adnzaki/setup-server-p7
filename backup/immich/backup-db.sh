c92b9f0c4900b3d0746544fb668f030ce9da5fad
Adnan Zaki <adnzaki@outlook.com>
2025-11-13 11:15:14 +0700

    fix mega destination folder

--- backup/immich/backup-db.sh
+++ backup/immich/backup-db.sh
@@ -6,10 +6,11 @@ DB_USER="postgres"
 CONTAINER="immich_postgres"
 MEGA_FOLDER="/immich/database"
 DATE_FOLDER=$(date +"%Y-%m-%d")
+MEGA_DEST_FOLDER="$MEGA_FOLDER/$DATE_FOLDER"
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M")
 BACKUP_FILE="immich_db_${TIMESTAMP}.sql"
 
-mega-mkdir -p "$MEGA_FOLDER/$DATE_FOLDER"
+mega-mkdir -p "$MEGA_DEST_FOLDER"
 
 echo "📦 Membuat backup database..."
 
@@ -17,7 +18,7 @@ echo "📦 Membuat backup database..."
 docker exec "$CONTAINER" pg_dump -U "$DB_USER" "$DB_NAME" > "/tmp/$BACKUP_FILE"
 
 # === Upload ke Mega ===
-mega-put "/tmp/$BACKUP_FILE" "$MEGA_FOLDER"
+mega-put "/tmp/$BACKUP_FILE" "$MEGA_DEST_FOLDER"
 
 # === Bersihkan file lokal ===
 rm "/tmp/$BACKUP_FILE"
