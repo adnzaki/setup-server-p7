28f7de48eb277d7a696a37da4acd9e51539d86f0
Adnan Zaki <adnzaki@outlook.com>
2025-11-13 11:04:39 +0700

    add immich backup folder permissions

--- update.sh
+++ update.sh
@@ -3,8 +3,10 @@ echo "🚀🚀🚀 Memulai update..."
 ACTIVE_USER=${SUDO_USER:-$(whoami)}
 USER_HOME=$(eval echo "~$ACTIVE_USER")
 BACKUP_FOLDER=$USER_HOME/setup-server-p7/backup
+IMMICH_BACKUP_FOLDER=$USER_HOME/immich
 
 git fetch && git pull
 chmod +x $BACKUP_FOLDER/*.sh
+chmod +x $IMMICH_BACKUP_FOLDER/*.sh
 
 echo "✅🏁 Update selesai. Permission pada $BACKUP_FOLDER telah diperbarui."
