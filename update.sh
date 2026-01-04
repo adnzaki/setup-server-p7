7b7fae4f606f00a543fc66c4e75da4b9ceee8f9e
Adnan Zaki <adnzaki@outlook.com>
2026-01-04 10:26:28 +0700

    fix permission update for nextcloud backup-data

--- update.sh
+++ update.sh
@@ -9,8 +9,8 @@ NEXTCLOUD_BACKUP_FOLDER=$USER_HOME/setup-server-p7/app/nextcloud
 git fetch && git pull
 chmod +x $BACKUP_FOLDER/*.sh
 chmod +x $IMMICH_BACKUP_FOLDER/*.sh
-chmod +x $NEXTCLOUD_BACKUP_FOLDER/backup-db.sh
+chmod +x $NEXTCLOUD_BACKUP_FOLDER/backup-data.sh
 
 echo "✅🏁 Update selesai. Permission pada $BACKUP_FOLDER telah diperbarui."
 echo "✅ Permission pada $IMMICH_BACKUP_FOLDER telah diperbarui."
-echo "✅ Permission pada $NEXTCLOUD_BACKUP_FOLDER/backup-db.sh telah diperbarui."
+echo "✅ Permission pada $NEXTCLOUD_BACKUP_FOLDER/backup-data.sh telah diperbarui."
