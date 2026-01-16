91ae5d8074576c39dd376d19d8b1c11ff6cd0d20
Adnan Zaki <adnzaki@outlook.com>
2026-01-16 15:53:57 +0700

    fix permission

--- update.sh
+++ update.sh
@@ -5,12 +5,16 @@ USER_HOME=$(eval echo "~$ACTIVE_USER")
 BACKUP_FOLDER=$USER_HOME/setup-server-p7/backup
 IMMICH_BACKUP_FOLDER=$BACKUP_FOLDER/immich
 NEXTCLOUD_BACKUP_FOLDER=$USER_HOME/setup-server-p7/app/nextcloud
+SISAUANG_BACKUP_FOLDER=$BACKUP_FOLDER/sisauang
 
 git fetch && git pull
 chmod +x $BACKUP_FOLDER/*.sh
 chmod +x $IMMICH_BACKUP_FOLDER/*.sh
 chmod +x $NEXTCLOUD_BACKUP_FOLDER/backup-data.sh
+chmod +x $NEXTCLOUD_BACKUP_FOLDER/backup-db.sh
+chmod +x $SISAUANG_BACKUP_FOLDER/*.sh
 
 echo "✅🏁 Update selesai. Permission pada $BACKUP_FOLDER telah diperbarui."
 echo "✅ Permission pada $IMMICH_BACKUP_FOLDER telah diperbarui."
-echo "✅ Permission pada $NEXTCLOUD_BACKUP_FOLDER/backup-data.sh telah diperbarui."
+echo "✅ Permission pada $NEXTCLOUD_BACKUP_FOLDER telah diperbarui."
+echo "✅ Permission pada $SISAUANG_BACKUP_FOLDER telah diperbarui."
