fda3bcbfc56b935b8d849df9d6cecec6f24490a7
Adnan Zaki <adnzaki@outlook.com>
2026-05-30 01:39:00 +0700

    add chmod to pengasinan7 backup folder

--- update.sh
+++ update.sh
@@ -6,6 +6,7 @@ BACKUP_FOLDER=$USER_HOME/setup-server-p7/backup
 IMMICH_BACKUP_FOLDER=$BACKUP_FOLDER/immich
 NEXTCLOUD_BACKUP_FOLDER=$USER_HOME/setup-server-p7/app/nextcloud
 SISAUANG_BACKUP_FOLDER=$BACKUP_FOLDER/sisauang
+PENGASINAN7_BACKUP_FOLDER=$BACKUP_FOLDER/pengasinan7
 
 git fetch && git pull
 chmod +x $BACKUP_FOLDER/*.sh
@@ -13,8 +14,9 @@ chmod +x $IMMICH_BACKUP_FOLDER/*.sh
 chmod +x $NEXTCLOUD_BACKUP_FOLDER/backup-data.sh
 chmod +x $NEXTCLOUD_BACKUP_FOLDER/backup-db.sh
 chmod +x $SISAUANG_BACKUP_FOLDER/*.sh
-
+chmod +x $PENGASINAN7_BACKUP_FOLDER/*.sh
 echo "✅🏁 Update selesai. Permission pada $BACKUP_FOLDER telah diperbarui."
 echo "✅ Permission pada $IMMICH_BACKUP_FOLDER telah diperbarui."
 echo "✅ Permission pada $NEXTCLOUD_BACKUP_FOLDER telah diperbarui."
 echo "✅ Permission pada $SISAUANG_BACKUP_FOLDER telah diperbarui."
+echo "✅ Permission pada $PENGASINAN7_BACKUP_FOLDER telah diperbarui."
